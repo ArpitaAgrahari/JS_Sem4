51bbab51b7c80af8f462e44f0bb28d0765504f57
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2023-12-21 18:33:37 +0530

    Updated Commit

--- arrayMethods.js
+++ arrayMethods.js
@@ -16,7 +16,57 @@
 
 
 
-                                               
+
+//join seperator used to append between elements like "and","-", " &" etc
+
+// let c = n.join(" and ");  //here and is a seperator
+// console.log(c);
+
+// let c = n.join(" & ");    //here & is a seperator
+// console.log(c);
+// let c = n.join(" _ ");    //here _ is a seperator
+// console.log(c);
+
+
+
+
+/* using of pop operations - basically it is used to pop out the ****last element**** which is being added */
+
+// let popelement = n.pop();
+// console.log(n );    
+// console.log("The poped element from the array is:", popelement);  //printing the pop element
+// console.log("Updated array : ",n);     //printing the updated array
+
+
+
+
+/* push operations - modifies the array */
+
+// let pushElement = n.push(23);
+// console.log(n);
+// console.log("The pushed element is :", pushElement);  //push updates the new array length
+// console.log("Updated array : ",n);              //updated array to be printed
+
+
+
+
+/* shift operator - delete the *****first element***** from the array and returns it */
+
+// let shiftElement = n.shift();
+// console.log(n);
+// console.log("Shifted Element : ",shiftElement);
+
+
+
+
+/*unshift operator - adds the new element in the array and upadted the array length */
+
+// let unshiftElement = n.unshift(23);
+// console.log(n);
+// console.log("Added element in the above array: ",unshiftElement);
+// console.log("Updated array:",n);
+
+                                    
 
 
 
