4ab26eddcf0b4a82677b048a0f531885fd1f2e66
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2023-12-21 11:42:22 +0530

    updated commit

---
+++ arrays.js
@@ -0,0 +1,10 @@
+
+
+// console.log("Arrays in JavaScript");
+// let class_marks = [89, 67, 98, 90, 78];
+// console.log(class_marks);
+
+
+
+
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+console.log("Arrays in JavaScript");
+// let class_marks = [89, 67, 98, 90, 78];
+// console.log(class_marks);
