3692036361a8f6959680c67c63b89545e10d6a3a
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2023-12-21 18:32:17 +0530

    Updated Commit

---
+++ arrayMethods.js
@@ -0,0 +1,25 @@
+// array methods for conversion
+
+/* used for conversion of elements to string */
+
+
+// let n = [2, 5, 8, 9, 6];
+// console.log(n); // simple array printing 
+
+
+
+
+// type conversion 
+
+// let b = n.toString();  // b is now string
+// console.log(b);
+
+
+
+                                               
+
+
+
+
+                                    
+
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,3 +1,1 @@
-console.log("Arrays in JavaScript");
-// let class_marks = [89, 67, 98, 90, 78];
-// console.log(class_marks);
+console.log(n); // simple array printing 
