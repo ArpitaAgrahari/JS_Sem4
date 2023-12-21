e7a4b728a46455b8cba9121f03c542ae3f268a6c
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2023-12-21 11:44:27 +0530

    Updated commit

--- arrays.js
+++ arrays.js
@@ -5,6 +5,25 @@
 // console.log(class_marks);
 
 
+/* Arrays always prints null values
+   when we fetch values which is not included in size that result in undefined value
+*/
+
+
+let class_marks = [89, 67, 98, 0,"absent", null, false];
+//  console.log(class_marks);
+
+// console.log(class_marks[0]);
+// console.log(class_marks[1]);
+// console.log(class_marks[2]);
+// console.log(class_marks[3]);
+// console.log(class_marks[4]);
+// console.log(class_marks[5]);
+// console.log(class_marks[6]); //not defined since index 6 is not defined
+// console.log(class_marks.length);
+// console.log("Given array has" ,class_marks.length ,"length.");
+
+
 
 
 
