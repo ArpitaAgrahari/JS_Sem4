e3649dbeacb71599bf60fbf2f3dfd3fcb5b69fa7
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2023-12-27 11:58:01 +0530

    Updated Commit

---
+++ LoopsInArray.js
@@ -0,0 +1,14 @@
+ 
+//Implementation of Loops in Array
+
+// let num = [23,56,78,98,45,29];
+//  for(let i = 0;i<num.length;i++){
+//      console.log(num[i]);
+//  }
+
+// //Another Unique method of above loop in JS ==== foreach loop
+
+//  num.forEach((element)=>{
+//      console.log(element*element);
+//  });
+
