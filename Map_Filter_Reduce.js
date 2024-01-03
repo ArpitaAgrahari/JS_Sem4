14b20ddcdcd9931c54857761ec3f4834db0bf57d
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-03 18:27:25 +0530

    Updated Commit

---
+++ Map_Filter_Reduce.js
@@ -0,0 +1,9 @@
+// MAP() - map makes new array by performing some operations and returns the array
+
+let arr = [4, 7, 8,9]
+let a = arr.map((element)=>{
+    console.log(element);
+    return element + 2;
+})
+// console.log(arr)
+console.log(a);
