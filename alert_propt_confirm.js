62e80e36000974e004b507b60c7685fdd4807a70
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-03 22:03:13 +0530

    Updated Commit

--- alert_propt_confirm.js
+++ alert_propt_confirm.js
@@ -1,1 +1,41 @@
-console.log("Hello")
+
+/* alert : used to invoke a mini window with a message. 
+    for example: alert("Hello")
+                 alert("Your file has been saved.")
+*/
+// alert("Your file has been saved to the database");
+// alert("Your Informatioon has been updated");
+
+
+
+/* Prompt: used to take input from the user as string.
+    for ex: inp = prompt("Enter the number");
+            name = prompt("Enter your name");
+*/
+// let a;
+// a = prompt("enter the number u want to add");
+// let name1;
+// name1 = prompt("Enter your name(first name):");
+
+
+
+/*Confirm : shows a message and waits for the user 
+   to press ok and cancel.Return strue for ok and false for cancel.
+*/
+// let write = confirm("Do you want to continue ")
+
+
+
+
+// just an example:
+
+// alert("Enter the value of a")
+// let a = prompt("Enter a here");
+// a = NUmber.parseInt(a);
+// alert("you entered a of type" + (typeof(a)))
+// let write1 = confirm("do you want tot write it to the page")
+// if(write1){
+// document.write(a)
+// }else{
+//     document.write("please allow me to write")
+// }
