34254d4697342b3183e30b79210220a04e46bd0d
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-03 19:20:29 +0530

    Updated Commit

---
+++ Practice.js
@@ -0,0 +1,48 @@
+ //Practice Set
+
+ //Ques1 : craetes an array of numbers and takes input from user to add numbers to this arary.
+// let arr = [ 1, 3, 5,8 ,9]
+// let a = prompt("Enter the Number u want to add to array")
+// a = Number.parseInt(a);
+// arr.push(a);
+// console.log(arr);
+
+
+ // Ques2: Keep adding numbers to the array in question1 until  0 is added to the array.
+//  let arr = [ 1, 3, 5,8 ,9]
+//  let a;
+// do{
+//   a = prompt("Enter the Number u want to add to array")
+//  a = Number.parseInt(a);
+//  arr.push(a);
+//  } while(a != 0);
+//  console.log(arr);
+
+
+
+ //ques3: filter fo numbers divisible by 10 from a given array.
+// let arr = [23,30,90,67,89,45,10]
+// let divisiblValue = arr.filter((value)=>{
+//     return value%10 == 0;
+// })
+// console.log(divisiblValue)
+
+
+
+ //Ques4: creates a array of square of given numbers.
+// let arr = [2,5,7,9,8]
+// let squareValue = arr.map((value)=>{
+//     return value*value;
+// })
+// console.log(squareValue)
+
+
+
+
+ // use reduce to calculate factorial of a given number from an array of fisrt 
+ //n natural numbers(n being number whose factorial needs to be calculated).
+//  let arr2 = [1,2,3,4,5,6];
+// let factorialValue =  arr2.reduce((num1,num2)=>{
+//     return num1*num2;
+//  })
+//  console.log(factorialValue)
