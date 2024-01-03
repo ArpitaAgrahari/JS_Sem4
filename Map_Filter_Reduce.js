ac4d0f78dc14e528e9d2e7b72fe7454294ea3c22
ArpitaAgrahari <arpitaagrahari004@gmail.com>
2024-01-03 18:54:01 +0530

    Updated Commit

--- Map_Filter_Reduce.js
+++ Map_Filter_Reduce.js
@@ -1,9 +1,56 @@
-// MAP() - map makes new array by performing some operations and returns the array
+//Array MAP() method - map makes new array by performing some operations and returns the array
 
-let arr = [4, 7, 8,9]
-let a = arr.map((element)=>{
-    console.log(element);
-    return element + 2;
-})
-// console.log(arr)
-console.log(a);
+// let arr = [4, 7, 8,9]
+// let a = arr.map((element)=>{
+//     console.log(element);
+//     return element + 2;
+// })
+// console.log(a);
+
+
+//example 2:
+// let arr1 = [3,5,7,0,1]
+// let a = arr1.map((value)=>{
+//     console.log(value);
+//     return value + 3;
+// })
+// console.log(a);
+
+//example 3:
+// let arr2 = [3.0,2.3,1.2];
+// let mappedValue = arr2.map((element)=>{
+//     console.log(element);
+//     return element + 2;
+// })
+// console.log(mappedValue)
+
+
+//Array Filter() method : filter an array with values thatt
+//                            passes a test and creates a new array.
+
+// let arr = [2,4,9,8,7,6,5];
+// let filteredValue = arr.filter((element)=>{
+//     return element<7;
+// })
+// console.log(filteredValue)
+
+// example 2:
+// let arr2 = [89, 98,56,45,34,23,12]
+// let filteredVale2 = arr2.filter((value)=>{
+//     return value<50;
+// })
+// console.log(filteredVale2);
+
+
+
+// Array Reduce() Method:  
+
+// let arr3 = [1, 4,6 ,7,8,3,1]
+// let reducedValue = arr3.reduce((value1,value2)=>{
+//     return value1 + value2;
+// })               //basically it first takes two values from the arary and 
+//                  //  adds them together athen what the value came after additon
+//                  // acts as value 1and again it takes another value from array and adds
+//                  // it with additon solution value .for ex: (value 1+value 2) i.e(1 + 4) = 5 then again value 1 becomes 5 and takes 6 i.e (5+6) = (11+7) and son on gives 30 as answer
+// console.log(reducedValue);
+
